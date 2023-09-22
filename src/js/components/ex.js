a52ccfc94a8036333f11d6318917f7a0e0d06f1f
Киленников Роман <kilennikovroman@gmail.com>
2023-09-22 17:07:11 +0300

    add catalog

--- src/js/components/ex.js
+++ src/js/components/ex.js
@@ -1,1 +1,18 @@
-console.log('maxgraph');
+const filterBtn = document.querySelector('.filter-open');
+const filter = document.querySelector('.filter');
+
+filterBtn.addEventListener('click', () => {
+  filter.classList.toggle('filter--open-js');
+})
+
+document.addEventListener('click', function (event) {
+  if (event.target.closest('.filter-open') == null && event.target.closest('.filter') == null) {
+    filter.classList.remove('filter--open-js');
+  }
+})
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 20) {
+    filter.classList.remove('filter--open-js');
+  }
+}, { passive: true });
